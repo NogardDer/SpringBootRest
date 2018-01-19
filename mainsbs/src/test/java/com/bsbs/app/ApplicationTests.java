f7e71e31d6fbdb59ccdae23b19840a04eb536957
Volodymyr Severenchuk <raritet.vvs@gmail.com>
2018-01-19 16:17:33 +0100

    Change test folder name

--- mainsbs/src/test/java/com/bsbs/app/ApplicationTests.java
+++ mainsbs/src/test/java/com/bsbs/app/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bsbs.app;
+package com.bsbs.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
